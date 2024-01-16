c4466e8b337c90f7dbf709aba4c87cbfda405b94
Cyrathec <40610788+Cyrathec@users.noreply.github.com>
2024-01-16 16:46:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 apt-get -y install make gcc dkms linux-source linux-headers-$(uname -r) build-essential neofetch gparted breeze-cursor-theme arc-theme papirus-icon-theme open-vm-tools-desktop open-vm-tools git fonts-cantarell fonts-firacode xfce4-panel-profiles mate-terminal
+mkdir -p /etc/skel/.config/xfce4/panel
+cp *.rs /etc/skel/.config/xfce4/panel/
+cp CustomBashrc /etc/skel/.bashrc
+cp CustomPanel.tar.bz2 /usr/share/xfce4-panel-profiles/layout
 xfconf-query -c xsettings -p /Net/ThemeName -s "Arc-Dark"
 xfconf-query -c xsettings -p /Net/IconThemeName -s "ePapirus-Dark"
 xfconf-query -c xfwm4 -p /general/workspace_names -s 1 -s 2 -s 3 -s 4
 update-alternatives --remove x-terminal-emulator /usr/bin/mate-terminal
 update-alternatives --install  /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/mate-terminal 100
+xfce4-panel-profiles load CustomPanel
+
+# Here need to move all the files in /etc/skel/ into the homedir of everyusers
+cp -r /etc/skel/* /root/
 
 echo "You may want to reboot to finalize the installation"
