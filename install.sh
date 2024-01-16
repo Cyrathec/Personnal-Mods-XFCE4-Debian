7782e48165f5f288b9050061fee2e934059974ea
Cyrathec <40610788+Cyrathec@users.noreply.github.com>
2024-01-16 13:45:26 +0100

    Update install.sh
    Added some utils for panels

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 apt-get -y install make gcc dkms linux-source linux-headers-$(uname -r) build-essential neofetch gparted breeze-cursor-theme arc-theme papirus-icon-theme open-vm-tools-desktop open-vm-tools git fonts-cantarell fonts-firacode xfce4-panel-profiles mate-terminal
+mkdir -R /etc/skel/.config/xfce4/panel/
+cp *.rc /etc/skel/.config/xfce4/panel/
+cp CustomPanel.tar.bz2 /usr/share/xfce4-panel-profiles/layouts/
 xfconf-query -c xsettings -p /Net/ThemeName -s "Arc-Dark"
 xfconf-query -c xsettings -p /Net/IconThemeName -s "ePapirus-Dark"
 xfconf-query -c xfwm4 -p /general/workspace_names -s 1 -s 2 -s 3 -s 4
