cbb5b19ea1aaa8087184b0422160b352988551b9
Cyrathec <40610788+Cyrathec@users.noreply.github.com>
2024-02-10 17:01:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-apt-get -y install make gcc dkms linux-source linux-headers-$(uname -r) build-essential neofetch gparted breeze-cursor-theme arc-theme papirus-icon-theme open-vm-tools-desktop open-vm-tools git fonts-cantarell fonts-firacode xfce4-panel-profiles mate-terminal
+# This part is root part
+apt-get -y install make gcc dkms linux-source linux-headers-$(uname -r) build-essential neofetch gparted breeze-cursor-theme arc-theme papirus-icon-theme open-vm-tools-desktop open-vm-tools git fonts-cantarell fonts-firacode xfce4-panel-profiles xfce4-*-plugins qterminal qt5ct
 mkdir -p /etc/skel/.config/xfce4/panel
 cp *.rc /etc/skel/.config/xfce4/panel/
 cp CustomBashrc /etc/skel/.bashrc
 cp -r /etc/skel/.* /root/
 cp -r /etc/skel/.* `ls -d /home/*`
 cp CustomPanel.tar.bz2 /usr/share/xfce4-panel-profiles/layouts/
-xfconf-query -c xsettings -p /Net/ThemeName -s "Arc-Dark"
-xfconf-query -c xsettings -p /Net/IconThemeName -s "ePapirus-Dark"
-xfconf-query -c xfwm4 -p /general/workspace_names -s 1 -s 2 -s 3 -s 4
-update-alternatives --remove x-terminal-emulator /usr/bin/mate-terminal
-update-alternatives --install  /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/mate-terminal 100
-xfce4-panel-profiles load CustomPanel.tar.bz2
+#update-alternatives --remove x-terminal-emulator /usr/bin/mate-terminal
+#update-alternatives --install  /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/mate-terminal 100
+
+# This part is on the user part
+#xfconf-query -c xsettings -p /Net/ThemeName -s "Arc-Dark"
+#xfconf-query -c xsettings -p /Net/IconThemeName -s "ePapirus-Dark"
+#xfconf-query -c xfwm4 -p /general/workspace_names -s 1 -s 2 -s 3 -s 4
+#xfce4-panel-profiles load CustomPanel.tar.bz2
 
 echo "You may need to congigure some panels like the CPU, Network and Generic monitors"
 echo "You may want to reboot to finalize the installation"
